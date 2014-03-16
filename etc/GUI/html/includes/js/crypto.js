09751fb82dfa041b3428e3e22e38327574e63d40
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2014-03-16 01:40:38 -0400

    Added basic files html files
    The webpage can open csv files and read it.

---
+++ etc/GUI/html/includes/js/crypto.js
@@ -0,0 +1,89 @@
+$( document ).ready(function() {
+	var fileButton = $('#files');
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		fileButton[0].addEventListener('change', handleFileSelect, false);
+		
+		$("#selectFiles").click(function() {
+			fileButton.click();
+		});
+	} else {
+		fileButton.remove();
+		alert('The File APIs are not fully supported in this browser.');
+	}
+});
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+var frequencies = {};
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+	var len = files.length;
+	
+	if (len > 0) {
+		var readCount;
+		var reader = new FileReader();
+	
+		reader.onload = (function(e) {
+			var f = files[readCount-1];
+			var filename = escape(f.name);
+			
+			if (filename.toLowerCase().endsWith(".csv")) {
+				filename = filename.substring(0,filename.length-4);
+			
+				var text = reader.result;
+				var arrays = $.csv.toArrays(text);
+				frequencies[filename] = arrays;
+			}
+			
+			if (readCount < len) {
+				readCount += 1;
+				reader.readAsText(files[readCount-1]);
+			} else {
+				generateMenuFromData();
+			}
+		});
+	
+		readCount = 1;
+		reader.readAsText(files[readCount-1]);
+	}
+}
+
+function generateMenuFromData() {
+	// create container for csv files
+	var prop = []; 
+	
+	// get all the filenames
+	for (var property in frequencies) {
+		if (frequencies.hasOwnProperty(property)) {
+			prop.push(property);
+		}
+	}
+	
+	// remove duplicates
+	var uniqueNames = [];
+	$.each(prop, function(i, el){
+		if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+	});
+	
+	// sort them
+	uniqueNames.sort();
+	
+	// clear the menu
+	var lst = $('#list');
+	lst.empty();
+	
+	// add them to the menu
+	for (var i=0; i<uniqueNames.length; i+=1) {
+		lst.append('<div class="language" key="' + uniqueNames[i] + '">' + uniqueNames[i] + '</div>');
+	}
+	
+	// define their click events
+	$(".language").click(function() {
+	var key = $(this).attr("key");
+		var lang = frequencies[key];
+		
+	});
+}
