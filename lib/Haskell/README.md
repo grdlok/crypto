03d0abcf60d07e430db56bc0ce3f7a1b8a36f39c
solvents <wisalam@live.com>
2014-04-20 11:08:36 -0400

    Updated haskell implementation to use a supplied file for its language

---
+++ lib/Haskell/README.md
@@ -0,0 +1,4 @@
+Usage:
+ ./caesar langfile offset
+ Uses a csv file "langfile" and an offset "offset" to perform a shift cipher of
+ any standard input.
--- lib/Haskell/caesar.hs
+++ lib/Haskell/caesar.hs
@@ -1,16 +1,35 @@
 import Data.Char
 import System.Environment
+import Text.CSV
+import Data.Array
 
-rot :: Int -> Char -> Char
-rot n a
-  | ord a >= 97 && ord a <= 122 =
-      chr $ mod ((ord a) - 97 + n) 26 + 97
-  | ord a >= 65 && ord a <= 90 =
-      chr $ mod ((ord a) - 65 + n) 26 + 65
-  | otherwise = a
+rot n alpha a = 
+    case find([], alpha) of
+      Just (l,r) -> shift n (l ++ cycle (reverse alpha)) (r ++ cycle alpha)
+      Nothing -> a
+    where
+      find (l,x:r)
+          | x == a = Just (l,x:r)
+          | otherwise = find (x:l,r)
+      find (l,[]) = Nothing
+      shift n lh rh
+          | n > 0 = shift (n-1) lh $ tail rh
+          | n < 0 = shift (n+1) (tail lh) (head lh : rh)
+          | otherwise = head rh
 
-cipher n = map (rot n)
+cipher n alpha = map (rot n alpha)
+
+letters e = array (0, len) lst
+    where
+      len = length e - 1
+      symbols = map head e
+      (_, lst) = foldr (\next (c,lst) -> (c-1,(c,next):lst)) (len,[]) symbols
+
+letters2 = concat . (map head)
 
 main = do
-  (arg:args) <- getArgs
-  interact $ cipher (read arg :: Int)
+  [lang, disp] <- getArgs
+  langFile <- readFile lang
+  case parseCSV lang langFile of
+    Left e -> print e
+    Right e -> interact $ cipher (read disp :: Int) $ letters2 e
