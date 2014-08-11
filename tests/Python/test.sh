31dd8c05621dc14e0b07dde946113b04af77f932
grdlok <adam.ortiz@utoronto.ca>
2014-08-11 17:10:48 -0400

    test added to test.sh

--- tests/Python/test.sh
+++ tests/Python/test.sh
@@ -3,3 +3,7 @@ cd `dirname $0`
 echo '**************Test Caesar*****************'
 PYTHONPATH=../../lib/Python/:../../etc/Alphabet/Python/ python2.7 CaesarTest.py
 echo '**************Test Caesar*****************'
+echo '**************Test Vigenere***************'
+PYTHONPATH=../../lib/Python/:../../etc/Alphabet/Python/ python2.7 VigenereTest.py
+echo '**************Test Vigenere***************'
+
